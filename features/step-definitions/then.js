92a62c123c4d063ac86be9e1b5720fe3736b93e3
MicleFilimonov <mishafilimonov1990@gmail.com>
2025-06-20 00:37:35 +0300

    add comments

--- features/step-definitions/then.js
+++ features/step-definitions/then.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 
-
+// Проверка видиомсти 1-ого элементов в указанном месте (классе)
 Then("Я {string} {string} в {string}", async function (activity, element, location) {
 
     const object = this.pageObjects[location];
