f282e67e2c01ef0035ca37e9f2329cd5b69eec29
MicleFilimonov <mishafilimonov1990@gmail.com>
2025-06-20 00:43:16 +0300

    dopily

--- features/step-definitions/given.js
+++ features/step-definitions/given.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 Given('Я открываю {string}', async function (project) {
-      // Получаем URL прямо из process.env по ключу из фичи
+
       const url = process.env[project];
       await browser.url(url);
     }
